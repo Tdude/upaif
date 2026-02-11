df9a5cf43837d773a62ee9cfa935cd0730e5058c
Tdude <tibbemail@gmail.com>
2026-02-11 16:10:26 +0100

    Style Contact Form 7 and hide header icons when empty

--- header.php
+++ header.php
@@ -48,17 +48,30 @@
 				<a href="<?php echo esc_url( home_url( '/klasser' ) ); ?>"><?php esc_html_e( 'Anmälan', 'upaif' ); ?></a>
 			</div>
 
-			<div class="upaif-nav__icons">
-				<a class="upaif-icon-box" href="<?php echo esc_url( get_theme_mod( 'upaif_footer_instagram_url', 'https://instagram.com' ) ); ?>" target="_blank" rel="noopener noreferrer" aria-label="<?php esc_attr_e( 'Instagram', 'upaif' ); ?>">
-					<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.32-1.45 5.12-3.5h-10.25c.8 2.05 2.79 3.5 5.13 3.5z"/></svg>
-				</a>
-				<a class="upaif-icon-box" href="<?php echo esc_url( get_theme_mod( 'upaif_footer_facebook_url', 'https://facebook.com' ) ); ?>" target="_blank" rel="noopener noreferrer" aria-label="<?php esc_attr_e( 'Facebook', 'upaif' ); ?>">
-					<svg viewBox="0 0 24 24"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"/></svg>
-				</a>
-				<a class="upaif-icon-box" href="<?php echo esc_url( get_theme_mod( 'upaif_contact_url', home_url( '/kontakt' ) ) ); ?>" aria-label="<?php esc_attr_e( 'Contact', 'upaif' ); ?>">
-					<svg viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/></svg>
-				</a>
-			</div>
+			<?php
+			$upaif_instagram_url = trim( (string) get_theme_mod( 'upaif_footer_instagram_url', 'https://instagram.com' ) );
+			$upaif_facebook_url = trim( (string) get_theme_mod( 'upaif_footer_facebook_url', 'https://facebook.com' ) );
+			$upaif_contact_url = trim( (string) get_theme_mod( 'upaif_contact_url', home_url( '/kontakt' ) ) );
+			?>
+			<?php if ( $upaif_instagram_url || $upaif_facebook_url || $upaif_contact_url ) : ?>
+				<div class="upaif-nav__icons">
+					<?php if ( $upaif_instagram_url ) : ?>
+						<a class="upaif-icon-box" href="<?php echo esc_url( $upaif_instagram_url ); ?>" target="_blank" rel="noopener noreferrer" aria-label="<?php esc_attr_e( 'Instagram', 'upaif' ); ?>">
+							<svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.32-1.45 5.12-3.5h-10.25c.8 2.05 2.79 3.5 5.13 3.5z"/></svg>
+						</a>
+					<?php endif; ?>
+					<?php if ( $upaif_facebook_url ) : ?>
+						<a class="upaif-icon-box" href="<?php echo esc_url( $upaif_facebook_url ); ?>" target="_blank" rel="noopener noreferrer" aria-label="<?php esc_attr_e( 'Facebook', 'upaif' ); ?>">
+							<svg viewBox="0 0 24 24"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zM7 10h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"/></svg>
+						</a>
+					<?php endif; ?>
+					<?php if ( $upaif_contact_url ) : ?>
+						<a class="upaif-icon-box" href="<?php echo esc_url( $upaif_contact_url ); ?>" aria-label="<?php esc_attr_e( 'Contact', 'upaif' ); ?>">
+							<svg viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/></svg>
+						</a>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
 		</nav>
 
 		<div class="upaif-hero__content">
